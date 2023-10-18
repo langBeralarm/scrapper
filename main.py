c14727ab571aa6229cb1e559dfae6b13e906bc2b
langBeralarm <leonardo.lang@beralarm.ch>
2023-10-18 13:49:16 +0200

    Add custom TimedCompressionRotatingFileHandler

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import logging.config
 import os
 
-base_dir = os.path.dirname(os.path.realpath(__file__))
+import random
+import string
+import time
+
+from utils import TimedCompressionRotatingFileHandler
+
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
 LOGGING_CONFIG = {
     "version": 1,
@@ -19,26 +25,35 @@ LOGGING_CONFIG = {
             "class": "logging.StreamHandler",
             "stream": "ext://sys.stdout",
         },
-        # "default": {
-        #     "level": "INFO",
-        #     "formatter": "standard",
-        #     "class": "logging.handlers.TimedRotatingFileHandler",
-        #     "filename": f"{base_dir}/logs/atas.log",
-        #     "when": "midnight",
-        #     "backupCount": 365,
-        # },
+        "default": {
+            "level": "INFO",
+            "formatter": "standard",
+            "class": "utils.TimedCompressionRotatingFileHandler",
+            "filename": os.path.join(BASE_DIR, 'logs/atas', 'atas.log'),
+            "when": "S",  # "when": "midnight",
+            "interval": 30,
+            "backupCount": 3,
+            "utc": True,
+        },
     },
     "loggers": {
         "root": {
             "handlers": ["console"],
             "level": "INFO",
         },
-        # "": {
-        #     "handlers": ["console", "default"],
-        #     "level": "INFO",
-        # },
+        "": {
+            "handlers": ["console", "default"],
+            "level": "INFO",
+        },
     },
 }
+
+# defaultHandler = logging.handlers.TimedRotatingFileHandler(
+#     os.path.join(BASE_DIR, 'logs/atas', 'atas.log'),
+#     'midnight',
+#     backupCount=365
+# )
+# defaultHandler.rotator = rotator
 
 logging.config.dictConfig(LOGGING_CONFIG)
 logger = logging.getLogger(__name__)
@@ -54,3 +69,13 @@ def custom_add(x: int, y: int) -> int:
 if __name__ == "__main__":
     logger.info('This is a test!')
     res: int = custom_add(4, 5)  # pragma: no cover
+    t = TimedCompressionRotatingFileHandler(filename=os.path.join(BASE_DIR, 'logs/atas', 'atas.log'))
+    while True:
+        msg = ''.join(random.choices(string.ascii_letters, k=random.randint(5, 15)))
+        logger.info(msg)
+        current_time = int(time.time())
+        print(f'Rollover time: {((t.computeRollover(current_time) - current_time) / (24 * 60 * 60))}')
+        time.sleep(2)
+
+
+
--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
-import logging
+import gzip
+import logging.handlers
+import os
+import shutil
 
-from datetime import datetime, timezone
+from datetime import datetime, timedelta, timezone
 
 
 class UTCFormatter(logging.Formatter):
@@ -15,3 +18,39 @@ class UTCFormatter(logging.Formatter):
         # Return the creation time in the UTC timezone including the full time with the fractional seconds truncated
         # to milliseconds
         return datetime.fromtimestamp(record.created, tz=timezone.utc).isoformat(timespec='milliseconds')
+
+
+class TimedCompressionRotatingFileHandler(logging.handlers.TimedRotatingFileHandler):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.suffix = self.suffix + ".log"
+
+    def custom_rotator(self, source, dest):
+        with open(source, 'rb') as file_in:
+            with gzip.open(dest, 'wb') as file_out:
+                shutil.copyfileobj(file_in, file_out)
+        os.remove(source)
+
+    def custom_namer(self, name):
+        return name + ".gz"
+
+    def computeRollover(self, currentTime: int) -> int:
+        """
+        Work out the rollover time for the end of each month.
+        :param currentTime: int
+        :return:
+        """
+        r = super().computeRollover(currentTime)
+
+        current_date = datetime.now()
+        next_month = current_date.replace(day=28) + timedelta(days=4)
+        last_day = next_month - timedelta(days=next_month.day)
+
+        if current_date >= last_day:
+            return r
+        days_delta = (last_day - current_date).days
+        return r + (days_delta * (24 * 60 * 60))
+
+    rotator = custom_rotator
+    namer = custom_namer
