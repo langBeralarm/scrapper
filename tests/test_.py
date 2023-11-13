cb8a8eca182ccbe0571052aa0295c9c953d29c0c
langBeralarm <leonardo.lang@beralarm.ch>
2023-11-13 15:55:28 +0100

    test: Add tests for eval_task Celery Task

--- tests/test_.py
+++ tests/test_.py
@@ -1,5 +1,25 @@
-from main import custom_add
+from datetime import datetime
+from unittest import TestCase
+from unittest.mock import MagicMock, patch
+
+from scrapper.celery import eval_task
 
 
-def test_initial():
-    assert custom_add(2, 2) == 4
+class CeleryTasksTest(TestCase):
+    @patch("scrapper.celery.save_exec_times")
+    def test_eval_task_1(self, save_exec_times: MagicMock):
+        # Arrange
+        now: datetime = datetime.now()
+        # Act
+        eval_task(10, now)
+        # Assert
+        save_exec_times.assert_called_with(now, None)
+
+    @patch("scrapper.celery.save_exec_times")
+    def test_eval_task_2(self, save_exec_times: MagicMock):
+        # Arrange
+        now: datetime = datetime.now()
+        # Act
+        eval_task(100, now)
+        # Assert
+        save_exec_times.assert_called_with(now, None)
