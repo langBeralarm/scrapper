61d55a6740271ba1f1a1c6a3e8d16e4a58f8a932
langBeralarm <leonardo.lang@beralarm.ch>
2024-02-27 17:21:02 +0100

    feat: Add basic logging config

--- main.py
+++ main.py
@@ -1,3 +1,51 @@
+import logging.config
+import os
+
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+
+# Setup logging
+LOGGING_CONFIG = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "standard": {
+            # pylint: disable=C0301
+            "format": "%(asctime)s - [%(levelname)s] - %(name)s - %(pathname)s.(%(funcName)s):%(lineno)d - %(message)s",  # noqa: E501
+        },
+    },
+    "handlers": {
+        "console": {
+            "level": "INFO",
+            "formatter": "standard",
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stdout",
+        },
+        "default": {
+            "level": "INFO",
+            "formatter": "standard",
+            "class": "logging.handlers.TimedRotatingFileHandler",
+            "filename": os.path.join(BASE_DIR, "logs/general", "general.log"),
+            "when": "midnight",
+            "backupCount": 30,
+            "utc": True,
+        },
+    },
+    "loggers": {
+        "root": {
+            "handlers": ["console"],
+            "level": "INFO",
+        },
+        "": {
+            "handlers": ["console", "default"],
+            "level": "INFO",
+        },
+    },
+}
+
+logging.config.dictConfig(LOGGING_CONFIG)
+logger = logging.getLogger(__name__)
+
+
 def custom_add(x: int, y: int) -> int:
     """
     Adds two ints and returns the sum.
